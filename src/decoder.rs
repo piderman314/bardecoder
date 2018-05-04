1460e8337f3529d68e83f210cfed2e926e9c5cd7
Mark Arts <piderman+github@gmail.com>
2018-05-04 20:46:33 +0200

    Add some documentation to Decoder(Builder)

--- src/decoder.rs
+++ src/decoder.rs
@@ -17,16 +17,30 @@ impl<S, G, T> Decoder<S, G, T> {
     }
 }
 
+/// Create a default Decoder
+///
+/// It will use the following components:
+///
+/// * grayscale: ToLuma
+/// * threshold: BlockedMean
+///
+/// This is meant to provide a good balance between speed and accuracy
 pub fn default_decoder() -> Decoder<DynamicImage, GrayImage, GrayImage> {
     default_builder().build()
 }
 
+/// Builder struct to create a Decoder
+///
+/// Required elements are:
+/// * Grayscale
+/// * Threshold
 pub struct DecoderBuilder<S, G, T> {
     grayscale: Option<Box<Grayscale<S, G>>>,
     threshold: Option<Box<Threshold<G, T>>>,
 }
 
 impl<S, G, T> DecoderBuilder<S, G, T> {
+    /// Constructor; all fields initialized as None
     pub fn new() -> DecoderBuilder<S, G, T> {
         DecoderBuilder {
             grayscale: None,
@@ -34,16 +48,23 @@ impl<S, G, T> DecoderBuilder<S, G, T> {
         }
     }
 
+    /// Add Grayscale component
     pub fn grayscale(&mut self, grayscale: Box<Grayscale<S, G>>) -> &mut DecoderBuilder<S, G, T> {
         self.grayscale = Some(grayscale);
         self
     }
 
+    /// Add Threshold component
     pub fn threshold(&mut self, threshold: Box<Threshold<G, T>>) -> &mut DecoderBuilder<S, G, T> {
         self.threshold = Some(threshold);
         self
     }
 
+    /// Build actual Decoder
+    ///
+    /// # Panics
+    ///
+    /// Will panic if any of the required components are missing
     pub fn build(self) -> Decoder<S, G, T> {
         if self.grayscale.is_none() {
             panic!("Cannot build Decoder without Grayscale component");
@@ -60,6 +81,14 @@ impl<S, G, T> DecoderBuilder<S, G, T> {
     }
 }
 
+/// Create a default DecoderBuilder
+///
+/// It will use the following components:
+///
+/// * grayscale: ToLuma
+/// * threshold: BlockedMean
+///
+/// The builder can then be customised before creating the Decoder
 pub fn default_builder() -> DecoderBuilder<DynamicImage, GrayImage, GrayImage> {
     let mut db = DecoderBuilder::new();
 
