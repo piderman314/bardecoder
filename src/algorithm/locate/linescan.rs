ea94b92e0bc5dda5c84cf36894d9dbfe7539ae4d
Mark Arts <piderman+github@gmail.com>
2018-05-05 23:48:13 +0200

    Verify vertical and diagonal (crudely)

--- src/algorithm/locate/linescan.rs
+++ src/algorithm/locate/linescan.rs
@@ -25,7 +25,20 @@ impl Locate<GrayImage> for LineScan {
                 pattern.4 += 1;
             } else {
                 if pattern.looks_like_finder() {
-                    candidates.push(QRFinderPosition { x, y });
+                    let module_size =
+                        (pattern.0 + pattern.1 + pattern.2 + pattern.3 + pattern.4) as f64 / 7.0;
+
+                    let finder_x = x - (3.5 * module_size) as u32;
+                    let finder_y = y;
+
+                    if self.verify_vertical(threshold, finder_x, finder_y, module_size)
+                        && self.verify_diagonal(threshold, finder_x, finder_y, module_size)
+                    {
+                        candidates.push(QRFinderPosition {
+                            x: finder_x,
+                            y: finder_y,
+                        });
+                    }
                 }
 
                 last_pixel = p.data[0];
@@ -36,6 +49,86 @@ impl Locate<GrayImage> for LineScan {
         candidates
 
         //locations
+    }
+}
+
+impl LineScan {
+    fn verify_vertical(
+        &self,
+        threshold: &GrayImage,
+        finder_x: u32,
+        finder_y: u32,
+        module_size: f64,
+    ) -> bool {
+        let dims = threshold.dimensions();
+
+        if finder_y < 7 * module_size as u32 {
+            return false;
+        }
+
+        if dims.1 - finder_y < 7 * module_size as u32 {
+            return false;
+        }
+
+        let mut last_pixel = 127;
+        let mut pattern = QRFinderPattern::new();
+        for y in finder_y.saturating_sub(5 * module_size as u32)..finder_y + 5 * module_size as u32
+        {
+            let p = threshold.get_pixel(finder_x, y)[0];
+            if p == last_pixel {
+                pattern.4 += 1;
+            } else {
+                if pattern.looks_like_finder() {
+                    return true;
+                }
+
+                last_pixel = p;
+                pattern.slide();
+            }
+        }
+
+        false
+    }
+
+    fn verify_diagonal(
+        &self,
+        threshold: &GrayImage,
+        finder_x: u32,
+        finder_y: u32,
+        module_size: f64,
+    ) -> bool {
+        let dims = threshold.dimensions();
+
+        if finder_x < 7 * module_size as u32 {
+            return false;
+        }
+
+        if dims.0 - finder_x < 7 * module_size as u32 {
+            return false;
+        }
+
+        let mut last_pixel = 127;
+        let mut pattern = QRFinderPattern::new();
+        for x in finder_x.saturating_sub(5 * module_size as u32)..finder_x + 5 * module_size as u32
+        {
+            for y in
+                finder_y.saturating_sub(5 * module_size as u32)..finder_y + 5 * module_size as u32
+            {
+                let p = threshold.get_pixel(x, y)[0];
+                if p == last_pixel {
+                    pattern.4 += 1;
+                } else {
+                    if pattern.looks_like_finder() {
+                        return true;
+                    }
+
+                    last_pixel = p;
+                    pattern.slide();
+                }
+            }
+        }
+
+        false
     }
 }
 
