f7cc3d758a3abc88348d8773c5d117d0858eaae2
Mark Arts <piderman+github@gmail.com>
2018-05-23 19:51:12 +0200

    Add feature debug-images

--- src/algorithm/extract.rs
+++ src/algorithm/extract.rs
@@ -3,6 +3,14 @@ use qr::QRLocation;
 
 use image::GrayImage;
 
+#[cfg(feature = "debug-images")]
+use image::{DynamicImage, Rgb};
+
+#[cfg(feature = "debug-images")]
+use std::{
+    cmp::{max, min}, env::temp_dir, fs::create_dir_all,
+};
+
 pub trait Extract<T> {
     fn extract(&self, threshold: &T, locs: Vec<QRLocation>) -> Vec<QRData>;
 }
@@ -33,15 +41,48 @@ impl Extract<GrayImage> for QRExtractor {
 
             let mut data = vec![];
 
+            #[cfg(feature = "debug-images")]
+            let mut img = DynamicImage::ImageLuma8(threshold.clone()).to_rgb();
+
             for _ in 0..size {
                 let mut line = start.clone();
 
                 for _ in 0..size {
-                    data.push(threshold.get_pixel(line.x.round() as u32, line.y.round() as u32)[0]);
+                    let x = line.x.round() as u32;
+                    let y = line.y.round() as u32;
+                    let pixel = threshold.get_pixel(x, y)[0];
+
+                    #[cfg(feature = "debug-images")]
+                    {
+                        if pixel == 0 {
+                            for i in max(0, x.saturating_sub(2))..min(img.dimensions().0, x + 2) {
+                                for j in max(0, y.saturating_sub(2))..min(img.dimensions().0, y + 2)
+                                {
+                                    img.put_pixel(i, j, Rgb { data: [255, 0, 0] });
+                                }
+                            }
+                        }
+                    }
+
+                    data.push(pixel);
                     line = line + dx;
                 }
 
                 start = start + dy;
+            }
+
+            #[cfg(feature = "debug-images")]
+            {
+                let mut tmp = temp_dir();
+                tmp.push("bardecoder-debug-images");
+
+                if let Ok(_) = create_dir_all(tmp.clone()) {
+                    tmp.push("data.png");
+
+                    if let Ok(_) = DynamicImage::ImageRgb8(img).save(tmp.clone()) {
+                        debug!("Debug image with data pixels saved to {:?}", tmp);
+                    }
+                }
             }
 
             qr_data.push(QRData::new(data, loc.version));
