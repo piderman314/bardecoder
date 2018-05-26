7ccd84f7af07161105a7ee49342bcece55bdd827
Mark Arts <piderman+github@gmail.com>
2018-05-26 10:43:53 +0200

    Skip alignment patterns

--- src/qr/blocks.rs
+++ src/qr/blocks.rs
@@ -4,16 +4,17 @@ use qr::{QRData, QRError};
 pub fn blocks(data: &QRData, mask: Box<QRMask>) -> Result<Vec<Vec<u8>>, QRError> {
     let mut codewords = Codewords::new();
     let mut x = data.side - 1;
+    let loc = alignment_location(data.version)?;
 
     loop {
         let y_range = y_range(x, data.side);
 
         for y in y_range {
-            if is_data(data, x, y) {
+            if is_data(data, &loc, x, y) {
                 codewords.add_bit(mask(data, x, y));
             }
 
-            if is_data(data, x - 1, y) {
+            if is_data(data, &loc, x - 1, y) {
                 codewords.add_bit(mask(data, x - 1, y));
             }
         }
@@ -40,7 +41,7 @@ fn y_range(x: u32, side: u32) -> Box<Iterator<Item = u32>> {
     }
 }
 
-fn is_data(data: &QRData, x: u32, y: u32) -> bool {
+fn is_data(data: &QRData, loc: &AlignmentLocation, x: u32, y: u32) -> bool {
     // timing patterns
     if x == 6 || y == 6 {
         return false;
@@ -61,7 +62,59 @@ fn is_data(data: &QRData, x: u32, y: u32) -> bool {
         return false;
     }
 
+    if is_alignment_coord(loc, x) && is_alignment_coord(loc, y) {
+        return false;
+    }
+
     true
+}
+
+fn is_alignment_coord(loc: &AlignmentLocation, coord: u32) -> bool {
+    if coord - 4 % 6 <= 4 {
+        return true;
+    }
+
+    if coord < loc.start - 2 {
+        return false;
+    }
+
+    if (coord - loc.start + 2) % loc.step <= 4 {
+        return true;
+    }
+
+    false
+}
+
+fn alignment_location(version: u32) -> Result<AlignmentLocation, QRError> {
+    match version {
+        // no alignment patterns for version 1 but this saves some exception paths
+        1 => Ok(AlignmentLocation::new(1000, 1000)),
+
+        // only one alignment pattern for versions 2-6 but this saves some exception paths
+        2 => Ok(AlignmentLocation::new(18, 1000)),
+        3 => Ok(AlignmentLocation::new(22, 1000)),
+        4 => Ok(AlignmentLocation::new(26, 1000)),
+        5 => Ok(AlignmentLocation::new(30, 1000)),
+        6 => Ok(AlignmentLocation::new(34, 1000)),
+
+        7 => Ok(AlignmentLocation::new(22, 16)),
+        36 => Ok(AlignmentLocation::new(24, 26)),
+        _ => Err(QRError {
+            msg: format!("Unknown version {}", version),
+        }),
+    }
+}
+
+#[derive(Debug)]
+struct AlignmentLocation {
+    start: u32,
+    step: u32,
+}
+
+impl AlignmentLocation {
+    fn new(start: u32, step: u32) -> AlignmentLocation {
+        AlignmentLocation { start, step }
+    }
 }
 
 struct Codewords {
@@ -93,5 +146,31 @@ impl Codewords {
 
     fn codewords(self) -> Vec<u8> {
         self.codewords
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test_alignment_locs() {
+        let al = alignment_location(36).unwrap();
+        let side = 4 * 36 + 17;
+
+        for x in 0..side {
+            if (x >= 4 && x <= 8)
+                || (x >= 22 && x <= 26)
+                || (x >= 48 && x <= 52)
+                || (x >= 74 && x <= 78)
+                || (x >= 100 && x <= 104)
+                || (x >= 126 && x <= 130)
+                || (x >= 152 && x <= 156)
+            {
+                assert!(is_alignment_coord(&al, x));
+            } else {
+                assert!(!is_alignment_coord(&al, x));
+            }
+        }
     }
 }
