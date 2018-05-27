82b90b00b81f07e80bb16370cca852a665a86e87
Mark Arts <piderman+github@gmail.com>
2018-05-27 16:14:37 +0200

    Fix mask error

--- src/qr/format.rs
+++ src/qr/format.rs
@@ -143,10 +143,11 @@ fn error_correction(bytes: u8) -> Option<ECLevel> {
 }
 
 fn mask(bytes: u8) -> Option<Box<QRMask>> {
+    debug!("MASK {:03b}", bytes);
     match bytes {
         0b000 => qrmask(Box::new(|i, j| (i + j) % 2 == 0)),
         0b001 => qrmask(Box::new(|i, _| i % 2 == 0)),
-        0b010 => qrmask(Box::new(|_, j| j % 2 == 0)),
+        0b010 => qrmask(Box::new(|_, j| j % 3 == 0)),
         0b011 => qrmask(Box::new(|i, j| (i + j) % 3 == 0)),
         0b100 => qrmask(Box::new(|i, j| (i / 2 + j / 3) % 2 == 0)),
         0b101 => qrmask(Box::new(|i, j| (i * j) % 2 + (i * j) % 3 == 0)),
