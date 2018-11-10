c010b1df164e5f9d19fdde6bdd4af6f1a7cb91a7
Mark Arts <piderman+github@gmail.com>
2018-11-10 12:02:30 +0100

    'Hide' benchmarks behind feature so stable build doesn't fail on it

--- benches/image_benches.rs
+++ benches/image_benches.rs
@@ -1,29 +1,32 @@
-#![feature(test)]
+#![cfg_attr(feature = "benchmark", feature(test))]
 
-extern crate bardecoder;
-extern crate image;
-extern crate test;
+#[cfg(all(feature = "benchmark", test))]
+mod bench {
+    extern crate bardecoder;
+    extern crate image;
+    extern crate test;
 
-use test::Bencher;
+    use self::test::Bencher;
 
-#[bench]
-pub fn version1_example(b: &mut Bencher) {
-    bench_image("tests/images/version1_example.jpg", b);
-}
+    #[bench]
+    pub fn version1_example(b: &mut Bencher) {
+        bench_image("tests/images/version1_example.jpg", b);
+    }
 
-#[bench]
-pub fn version3_example2(b: &mut Bencher) {
-    bench_image("tests/images/version3_example2.jpg", b);
-}
+    #[bench]
+    pub fn version3_example2(b: &mut Bencher) {
+        bench_image("tests/images/version3_example2.jpg", b);
+    }
 
-#[bench]
-pub fn needs_alignment(b: &mut Bencher) {
-    bench_image("tests/images/needs_alignment.jpg", b);
-}
+    #[bench]
+    pub fn needs_alignment(b: &mut Bencher) {
+        bench_image("tests/images/needs_alignment.jpg", b);
+    }
 
-pub fn bench_image(file: &str, b: &mut Bencher) {
-    let img = image::open(file).unwrap();
-    let decoder = bardecoder::default_decoder();
+    pub fn bench_image(file: &str, b: &mut Bencher) {
+        let img = image::open(file).unwrap();
+        let decoder = bardecoder::default_decoder();
 
-    b.iter(|| decoder.decode(img.clone()))
+        b.iter(|| decoder.decode(img.clone()))
+    }
 }
