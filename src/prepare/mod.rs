f72b511cd3a560b0b06173bd8602ddda6637015b
Mark Arts <piderman+github@gmail.com>
2019-04-28 17:57:09 +0200

    Add Prepare doc

--- src/prepare/mod.rs
+++ src/prepare/mod.rs
@@ -4,6 +4,37 @@ mod blockedmean;
 
 pub use self::blockedmean::BlockedMean;
 
+/// Prepare the source image for data extraction, for example by converting it to black/white
+///
+/// IMG type should be the type of the source image
+/// PREPD type should be the type of the output image. It does not have to be the same as the IMG type
+///
+/// Pre-implemented Prepare provided by this library that are included in the default [`Decoder`]:
+/// * [`BlockedMean`]
+///
+/// # Example
+/// ```
+/// # extern crate bardecoder;
+/// # extern crate image;
+/// # use image::{DynamicImage, GrayImage};
+/// use bardecoder::prepare::Prepare;
+///
+/// struct MyPreparator {}
+///
+/// impl Prepare<DynamicImage, GrayImage> for MyPreparator {
+///     fn prepare(&self, input: DynamicImage) -> GrayImage {
+///         // prepare image here
+/// #       input.to_luma()
+///     }
+/// }
+/// ```
+///
+/// with the corresponding impl Prepare being the Example [`here`]
+///
+/// [`Decoder`]: ../struct.Decoder.html
+/// [`Detect`]: ../detect/trait.Detect.html
+/// [`here`]: ../prepare/trait.Prepare.html
 pub trait Prepare<IMG, PREPD> {
+    /// Does the actual preparing
     fn prepare(&self, source: IMG) -> PREPD;
 }
