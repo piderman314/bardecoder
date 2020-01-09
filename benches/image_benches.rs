15d604053b6b226fe676d3fe41e187662ceda47c
Mark Arts <piderman+github@gmail.com>
2020-01-09 12:19:25 +0100

    Also update benchmark to use image reference #12

--- benches/image_benches.rs
+++ benches/image_benches.rs
@@ -6,27 +6,31 @@ mod bench {
     extern crate image;
     extern crate test;
 
+    use image::DynamicImage;
+
     use self::test::Bencher;
 
     #[bench]
     pub fn version1_example(b: &mut Bencher) {
-        bench_image("tests/images/version1_example.jpg", b);
+        let img = image::open("tests/images/version1_example.jpg").unwrap();
+        bench_image(&img, b);
     }
 
     #[bench]
     pub fn version3_example2(b: &mut Bencher) {
-        bench_image("tests/images/version3_example2.jpg", b);
+        let img = image::open("tests/images/version3_example2.jpg").unwrap();
+        bench_image(&img, b);
     }
 
     #[bench]
     pub fn needs_alignment(b: &mut Bencher) {
-        bench_image("tests/images/needs_alignment.jpg", b);
+        let img = image::open("tests/images/needs_alignment.jpg").unwrap();
+        bench_image(&img, b);
     }
 
-    pub fn bench_image(file: &str, b: &mut Bencher) {
-        let img = image::open(file).unwrap();
+    pub fn bench_image(image: &DynamicImage, b: &mut Bencher) {
         let decoder = bardecoder::default_decoder();
 
-        b.iter(|| decoder.decode(img.clone()))
+        b.iter(|| decoder.decode(image))
     }
 }
